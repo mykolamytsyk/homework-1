d056e6847167faddf788296851ac836a331bedd6
mykolamytsyk <138970828+mykolamytsyk@users.noreply.github.com>
2024-01-01 18:27:34 +0200

    edfe

--- burger from game bulls/main.js
+++ burger from game bulls/main.js
@@ -34,79 +34,8 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-function checkAndReturn() {
-  let userInput = document.getElementById("userInput").value;
-
-  if (userInput.length === 4 && !hasRepeatedDigits(userInput)) {
-  } else {
-    alert("Будь ласка, введіть коректне чотиризначне число без повторень.");
-  }
-}
-
-// проверка четырехзначного и повторов от пользователя
-
-function hasRepeatedDigits(number) {
-  let digitCount = {};
-  for (let i = 0; i < number.length; i++) {
-    let digit = number[i];
-    if (digitCount[digit]) {
-      return true;
-    } else {
-      digitCount[digit] = true;
-    }
-  }
-  return false;
-}
-
 // очистка инпута
 
 function clearInput() {
   document.getElementById("userInput").value = "";
-}
-
-// function generateNumber() {
-//   // Згенерувати випадкове чотиризначне число
-//   var randomNumber = Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000;
-
-//   console.log(randomNumber);
-// }
-
-function generateUniqueNumber() {
-  let digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-  // Перемішуємо масив цифр
-  for (let i = digits.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1));
-    [digits[i], digits[j]] = [digits[j], digits[i]];
-  }
-
-  // Вибираємо перші чотири цифри
-  let generatedNumber = digits.slice(0, 4).join("");
-
-  return generatedNumber;
-}
-
-// Приклад використання:
-let randomNumber = generateUniqueNumber();
-// console.log(randomNumber);
-
-function generateAndCompare() {
-  var userInput = document.getElementById("userInput").value;
-
-  // Перевірка чотиризначності та відсутності повторюючихся цифр у введеному числі
-  if (userInput.length === 4 && !hasRepeatedDigits(userInput)) {
-    var generatedNumber = generateUniqueNumber();
-    return generatedNumber;
-    // Порівнюємо числа
-    if (userInput === generatedNumber) {
-      document.getElementById("result").textContent =
-        "Вітаємо! Ви вгадали число!";
-    } else {
-      document.getElementById("result").textContent =
-        "Спробуйте ще раз. Згенероване число: " + generatedNumber;
-    }
-  } else {
-    document.getElementById("result").textContent =
-      "Будь ласка, введіть коректне чотиризначне число без повторень.";
-  }
 }
